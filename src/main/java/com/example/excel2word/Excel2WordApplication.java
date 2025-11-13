246f1b5f8d2bb9d96403a6912e4e6298dc3f14ba
trankhachuu <trankhachuu123@gmail.com>
2025-11-13 13:33:14 +0800

    first commit

---
+++ src/main/java/com/example/excel2word/Excel2WordApplication.java
@@ -0,0 +1,11 @@
+package com.example.excel2word;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Excel2WordApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Excel2WordApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/excel2word/service/DocService.java
@@ -0,0 +1,34 @@
+package com.example.excel2word.service;
+
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.*;
+import org.springframework.stereotype.Service;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Service
+public class DocService {
+
+    public Path createDocFromRow(String stt, String ngayTl, Path outputDir) throws Exception {
+        if (!Files.exists(outputDir)) Files.createDirectories(outputDir);
+        String fileName = String.format("STT_%s_NgayTL_%s.doc", sanitize(stt), sanitize(ngayTl));
+        Path out = outputDir.resolve(fileName);
+
+        // Create simple .doc using HWPFDocument
+        try (POIFSFileSystem fs = new POIFSFileSystem();
+             HWPFDocument doc = new HWPFDocument(fs)) {
+            doc.getRange().insertAfter("STT: " + stt + "\n" + "Ngày TL: " + ngayTl + "\n");
+            try (FileOutputStream fos = new FileOutputStream(out.toFile())) {
+                doc.write(fos);
+            }
+        }
+        return out;
+    }
+
+    private String sanitize(String s) {
+        return s.replaceAll("[\\/:*?<>|]", "_").replaceAll("\\s", "_");
+    }
+}
